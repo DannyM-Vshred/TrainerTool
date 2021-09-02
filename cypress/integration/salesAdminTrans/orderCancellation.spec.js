1974dc97680e52ae07ae08453675e1c34fd03c04
DannyM-Vshred <83987327+DannyM-Vshred@users.noreply.github.com>
2021-09-02 16:15:06 -0700

    Trainer test (#7)
    * testSync
    
    * updates to assignedClients Beta pageTest
    
    * veh_190
    
    * updates to VEH190 spec
    
    * update filter by Trainer logic
    
    * draft scripts to suspend and reinstate trainers
    
    * add script to be able to Unselect a CDP plan
    
    * added script while learning webElement
    
    * added custom command to UploadFile
    
    * minor changes to e2e scripts
    
    * learning notes only
    
    * script updates
    
    * temp
    
    * Added TrainerLink purchase regression spec
    
    * clean up purchase regression scripts
    
    * Adding purchasing bundles in admin
    
    * void Order with Paid and Fulfilled Order status

---
+++ cypress/integration/salesAdminTrans/orderCancellation.spec.js
@@ -0,0 +1,143 @@
+/// <reference types = "Cypress" /> 
+import 'cypress-iframe';
+
+describe('Repurchase Bundle through Sales Admin',()=>{
+
+    before(()=>{
+        cy.fixture('loginData').as('loginData');
+        cy.viewport(1920, 1080)
+        cy.visit('/')
+
+    })
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        // returning false here prevents Cypress from
+        // failing the test
+        return false
+        });
+
+    const searchMail = 'betaTest01otpCdpHim0901@example.net'
+    // const searchMail = 'test04newmember06012021@mailinator.com' // multiple orders
+    // const searchMail = 'cyTest20otpCdpHim0827@example.net'      //'cyTest11otp6PackShred0825@example.net'
+    // const searchMail = 'tests+gi+1592159600422@vshred.com'
+    const order_id = 'c35a4593-5962-4711-b887-8908101853ca'
+
+    it('can open Existing Orders',()=>{    
+        //login as SalesAgent
+        cy.get('#menu1').contains('Login').click()
+        cy.get('@loginData').then(login=>{
+            cy.get('#email').clear().type(login[3].email);
+            cy.get('#password').type(login[3].password)
+            cy.get('[type="submit"]').click()
+            cy.contains('Welcome to the admin dashboard').should('be.visible');
+        })
+
+        cy.contains('Users').click()
+        cy.url().should('include','/admin/users')
+
+        // search existing user
+        cy.get('#__BVID__16').clear().type(searchMail +'{enter}')
+        cy.contains('.vuetable-body td', searchMail)
+            .should('exist')
+            .parent()
+            .within($tr => {
+                cy.get('button[title=View]').click()
+            })
+
+        //select Purchase tab
+        cy.get('ul[role=tablist]').contains('Purchases')
+            .click()        
+            .should('have.attr', 'aria-selected')
+            .and('include', 'true')
+        cy.wait(2000)
+
+        //get order record with status=Fulfilled or Paid
+        cy.contains('tbody .th-name','Custom').then(($cdp)=>{
+            cy.wrap($cdp).prev().invoke('text').then((status)=>{
+                const orderStatus = status
+                cy.log('Order Status: '+ orderStatus)
+                
+                if((orderStatus==="\n Paid\n")||(orderStatus==="\n Fulfilled\n")){
+                    // case "\n Paid\n":
+                        cy.wrap($cdp).parent().within(($th)=>{
+                            cy.wrap($th).get('.th-id').invoke('text').then((order)=>{
+                                cy.log('OrderId: ' + order)    
+                                cy.get('.th-id').click()
+                                cy.wait(2000)
+                                cy.url().should('contain', '/process-order/'+order.trim())
+                            })
+                            
+                        })
+
+                    cy.get('tbody td').then(($orderCart) => {
+                    cy.wrap($orderCart).contains('Custom').should('be.visible')
+
+                    //check correct record is displayed
+                    cy.get('.wrapper.wrapper-content .card-body')
+                        .within(($str) => {
+                            cy.wrap($str).contains(searchMail).should('be.visible')
+                        })
+
+                    //check Payment status is 'Submitted for Settlement'
+                    cy.get('#__BVID__55 td[aria-colindex=4]')
+                        .then(($payments) => {
+                            cy.wrap($payments)  //.contains('td', 'Submitted For Settlement')
+                                .should('contain.text', 'Submitted For Settlement')
+                                .next()
+                                .should('contain.text', 'true')
+                        })
+
+                    //Void order    
+                    cy.get('button').contains('Void')
+                        .should('be.visible').and('be.enabled')
+                        .click()
+
+                    cy.get('#voidOrderModal___BV_modal_content_').should('contain.text', 'Confirm that you would like to void this order')
+                    cy.get('#voidOrderModal___BV_modal_footer_ button').contains('Void').click()
+                    cy.wait(1000)
+                    cy.get('.toast-success .toast-message').should('contain.text', 'successfully voided')
+
+                    // check Payment section
+                    cy.get('#__BVID__55 tbody')
+                        .then(($payments) => {
+                            cy.wrap($payments).contains('td', 'Voided')
+                                .should('be.visible')
+                                .next()
+                                .should('contain.text', 'false')
+                        })
+                    cy.wait(1500)
+
+                    //move back to users view
+                    cy.get('.wrapper.wrapper-content .card-body').contains(searchMail)
+                        .then(($email) => {
+                            cy.wrap($email).find('a').click()
+                            cy.url().should('contain', '/admin/users/')
+                        })
+
+                    //select Purchase tab
+                    cy.get('ul[role=tablist]').contains('Purchases')
+                        .click()
+                        .should('have.attr', 'aria-selected')
+                        .and('include', 'true')
+                    cy.wait(2000)
+
+                    //get order record with status=Fulfilled or Paid   
+                    cy.contains('tbody .th-name', 'Custom').then(($cdp) => {
+                        cy.wrap($cdp).prev()
+                            .should('contain.text', 'Canceled')
+                            .invoke('text')
+                            .then((status) => {
+                                const orderStatus = status
+                                cy.log('Order Status: ' + orderStatus)
+                            })
+                    })
+                })
+
+                } else {
+                    cy.log('Order is not for cancellation')
+                    
+                }
+                
+            })
+        })
+    })
+})
