d949c62bfc50d2cde8b4051251af4939f4592119
DannyM-Vshred <83987327+DannyM-Vshred@users.noreply.github.com>
2021-09-16 13:52:49 -0700

    Trainer test (#11)
    * testSync
    
    * updates to assignedClients Beta pageTest
    
    * veh_190
    
    * updates to VEH190 spec
    
    * update filter by Trainer logic
    
    * draft scripts to suspend and reinstate trainers
    
    * add script to be able to Unselect a CDP plan
    
    * added script while learning webElement
    
    * added custom command to UploadFile
    
    * minor changes to e2e scripts
    
    * learning notes only
    
    * script updates
    
    * temp
    
    * Added TrainerLink purchase regression spec
    
    * clean up purchase regression scripts
    
    * Adding purchasing bundles in admin
    
    * void Order with Paid and Fulfilled Order status
    
    * update to diet preference element locator
    
    * additional features and updates to skipping video
    
    * Added supplement only purchase regression tests
    
    * boost upsell spec added
    
    * updated SalesAgent bundlePurchase

--- cypress/integration/salesAdminTrans/newBundlePurchase.spec.js
+++ cypress/integration/salesAdminTrans/newBundlePurchase.spec.js
@@ -4,220 +4,222 @@ import 'cypress-iframe';
 describe('new Bundle purchase through sales Admin', () => {
 
     before(() => {
+        cy.fixture('supplements/customBundle').as('bundle');
         cy.fixture('loginData').as('loginData');
         cy.viewport(1920, 1080)
         cy.visit('/')
 
     })
+
     Cypress.on('uncaught:exception', (err, runnable) => {
         // returning false here prevents Cypress from
         // failing the test
         return false
     });
 
-    const ctr = '15'
-    const fName = 'cyBundle' + ctr
-    const lName = 'newMember0827'
-    const newMemberUser = fName + " " + lName
-    const newMemberEmail = fName + lName + '@example.net'
-    const assignTrainer = 'cyTrainer OTP'
+    const ctr = '32'
+    const dateS = '0615';
+    const cName = 'cyBundle'
+    const assignTrainer = 'beta TrainerBundle'
 
-    it('can create Bundle Purchase for New Member users through Admin', () => {
-        //login as SalesAgent
-        cy.get('#menu1').contains('Login').click()
-        cy.get('@loginData').then(login => {
-            cy.get('#email').clear().type(login[3].email);
-            cy.get('#password').type(login[3].password)
-            cy.get('[type="submit"]').click()
-            cy.contains('Welcome to the admin dashboard').should('be.visible');
-        })
+    it('can create Bundle with Custom Plans via Sales Agent', function() {                                                                                                                              
+        const customBundle = this.bundle
+        const salesAgentlogin = this.loginData
 
-        cy.contains('Users').click()
-        cy.url().should('include', '/admin/users')
+        cy.get(customBundle).each((searchBundle) => {
+            const fName = cName + ctr
+            const lName = searchBundle.customBundleOrder + dateS
+            const cEmail = fName + lName + '@example.net'
+            
+            cy.visit('/')
 
-        // create new Member user
-        cy.createNewMemberUser(
-            {
-                name: newMemberUser,
-                email: newMemberEmail
+            //login as SalesAgent
+            cy.get('#menu1').contains('Login').click()
+            cy.get(salesAgentlogin).then(login => {
+                cy.get('#email').clear().type(login[3].email);
+                cy.get('#password').type(login[3].password)
+                cy.get('[type="submit"]').click()
+                cy.contains('Welcome to the admin dashboard').should('be.visible');
             })
 
-        //Purchase tab
-        cy.get('ul[role=tablist]').contains('Purchases')
-            .click()
-            .should('have.attr', 'aria-selected')
-            .and('include', 'true')
-        cy.wait(2000)
+            cy.contains('Users').click()
+            cy.url().should('include', '/admin/users')
 
-        //Creating new Order
-        cy.get('#new-order').click()
-        cy.url().should('include', '/process-order/')
+            // create new Member user
+            cy.createNewMemberUser(
+                {
+                    name: fName + " " + lName,
+                    email: cEmail
+                })
 
-        // add new Shipping address
-        cy.addNewShipBillAddress(
-            {
-                shipname: newMemberUser
+            //Purchase tab
+            cy.get('ul[role=tablist]').contains('Purchases')
+                .click()
+                .should('have.attr', 'aria-selected')
+                .and('include', 'true')
+            cy.wait(2000)
+
+            //Creating new Order
+            cy.get('#new-order').click()
+            cy.url().should('include', '/process-order/')
+
+            // add new Shipping address
+            cy.addNewShipBillAddress({ shipname: cName })
+
+            //Add order Cart   
+            cy.get('button').contains('Add Offer').click()
+
+            //select Bundles
+            cy.get('#addOfferModal___BV_modal_body_ li')
+                .then(($tr) => {
+                    cy.wrap($tr).find('a').contains('Bundles').click()
+                })
+            //search known bundle
+            cy.get('input[placeholder="Search by Title or Description"]').clear().type(searchBundle.custmoBundleSearch)
+            cy.contains('td', searchBundle.cartSummary)
+                .should('exist')
+                .parent()
+                .within($tr => {
+                    cy.get('button').contains('Add Bundle').click()
+                })
+
+            cy.wait(2000)
+            //review Cart details
+            cy.get('tbody td').then(($orderCart) => {
+                cy.wrap($orderCart).contains(searchBundle.cartOrder1).should('be.visible')
+                cy.wrap($orderCart).contains(searchBundle.cartOrder2).should('be.visible')
+                
             })
 
-        //Add order Cart   
-        cy.get('button').contains('Add Offer').click()
+            cy.get('button').contains('Place Order').click()
+            cy.wait(2000)
 
-        //select Bundles
-        cy.get('#addOfferModal___BV_modal_body_ li')
-            .then(($tr) => {
-                cy.wrap($tr).find('a').contains('Bundles').click()
-            })
-        //search known bundle
-        cy.get('input[placeholder="Search by Title or Description"]').clear().type('last')
-        cy.contains('td', 'Last Ditch')
-            .should('exist')
-            .parent()
-            .within($tr => {
-                cy.get('button').contains('Add Bundle').click()
-            })
+            cy.get('#paymentModal___BV_modal_body_')
+                .then(($payCart) => {
+                    cy.wrap($payCart).contains(searchBundle.cartSummary).should('be.visible')
+                })
+            cy.wait(5000)
 
-        cy.wait(2000)
-        //review Cart details
-        cy.get('tbody td').then(($orderCart) => {
-            cy.wrap($orderCart).contains("I'm Worth It Custom Diet Plan and One Bottle of Burn Bundle").should('be.visible')
-            cy.wrap($orderCart).contains('Burn Evolved').should('be.visible')
-            cy.wrap($orderCart).contains('Custom Diet Plan - 8 Week').should('be.visible')
-        })
+            cy.get('.braintree-cc a').contains('New Card').click()
+            //enter payment info
+            cy.enterPaymentInfoAdmin()
 
-        cy.get('button').contains('Place Order').click()
-        cy.wait(2000)
-
-        cy.get('#paymentModal___BV_modal_body_')
-            .then(($payCart) => {
-                cy.wrap($payCart).contains("I'm Worth It Custom Diet Plan and One Bottle of Burn Bundle").should('be.visible')
-            })
-        cy.wait(5000)
-
-        cy.get('.braintree-cc a').contains('New Card').click()
-        //enter payment info
-        cy.enterPaymentInfoAdmin()
-
-        //check Payment section
-        cy.get('#__BVID__55 tbody')
-            .then(($payments) => {
-                cy.wrap($payments).contains('td', 'Submitted For Settlement')
-                    .should('be.visible')
-                    .next()
-                    .should('contain.text', 'true')
-            })
-        cy.wait(1500)
-
-        //new client
-        cy.get('.wrapper.wrapper-content .card-body')
-            .then(($OrderDetails) => {
-                cy.wrap($OrderDetails).within(($str) => {
-                    cy.wrap($str).contains(newMemberEmail)
+            //check Payment section
+            cy.get('#__BVID__55 tbody')
+                .then(($payments) => {
+                    cy.wrap($payments).contains('td', 'Submitted For Settlement')
                         .should('be.visible')
+                        .next()
+                        .should('contain.text', 'true')
                 })
-            })
+            cy.wait(1500)
 
-        cy.get('button').contains('Void').should('be.visible').and('be.enabled')
-
-
-        //search newly created user
-        cy.contains('Users').click()
-        cy.url().should('include', '/admin/users')
-
-        // search existing user
-        cy.get('#__BVID__16').clear().type(newMemberEmail + '{enter}')
-        cy.contains('.vuetable-body td', newMemberEmail)
-            .should('exist')
-            .parent()
-            .within($tr => {
-                cy.get('button[title=View]').click()
-            })
-
-        //Purchase tab
-        cy.get('ul[role=tablist]').contains('Purchases')
-            .click()
-            .should('have.attr', 'aria-selected')
-            .and('include', 'true')
-        cy.wait(2000)
-
-        //Open existing order
-        cy.get('tbody td').contains("I'm Worth It Custom Diet Plan and One Bottle of Burn Bundle")
-            .should('be.visible')
-            .parent()
-            .within(($str)=>{
-                cy.wrap($str).get('.th-id a').invoke('text').then((text)=>{
-                    const orderID = text
-                    cy.log('OrderId: ' + orderID)
+            //new client
+            cy.get('.wrapper.wrapper-content .card-body')
+                .then(($OrderDetails) => {
+                    cy.wrap($OrderDetails).within(($str) => {
+                        cy.wrap($str).contains(cEmail)
+                            .should('be.visible')
+                    })
                 })
-                cy.wrap($str).get('.th-status')
-                    .should('contain.text','Paid')
-                    .invoke('text').then((text)=>{
-                    const orderStatus = text
-                    cy.log('OrderId: ' + orderStatus)
+
+            cy.get('button').contains('Void').should('be.visible').and('be.enabled')
+
+
+            //search newly created user
+            cy.contains('Users').click()
+            cy.url().should('include', '/admin/users')
+
+            // search existing user
+            cy.get('#__BVID__16').clear().type(cEmail + '{enter}')
+            cy.contains('.vuetable-body td', cEmail)
+                .should('exist')
+                .parent()
+                .within($tr => {
+                    cy.get('button[title=View]').click()
                 })
-          })  
 
-        cy.wait(2000)
-        //logout as Sales Agent
-        cy.get('.navbar-top-links a').contains('Logout').click()
+            //Purchase tab
+            cy.get('ul[role=tablist]').contains('Purchases')
+                .click()
+                .should('have.attr', 'aria-selected')
+                .and('include', 'true')
+            cy.wait(2000)
 
-        //login as Trainer Manager
-        cy.get('.btn__text').contains('Login').click()
-        cy.loginTrainerManager()
+            //Open existing order
+            cy.get('tbody td').contains(searchBundle.cartSummary)
+                .should('be.visible')
+                .parent()
+                .within(($str) => {
+                    cy.wrap($str).get('.th-id a').invoke('text').then((text) => {
+                        const orderID = text
+                        cy.log('OrderId: ' + orderID)
+                    })
+                    cy.wrap($str).get('.th-status')
+                        .should('contain.text', 'Paid')
+                        .invoke('text').then((text) => {
+                            const orderStatus = text
+                            cy.log('OrderId: ' + orderStatus)
+                        })
+                })
 
-        cy.contains('Users').click()
-        cy.url().should('include', '/admin/users')
+            cy.wait(2000)
+            //logout as Sales Agent
+            cy.get('.navbar-top-links a').contains('Logout').click()
 
-        // search existing user
-        cy.get('#__BVID__16').clear().type(newMemberEmail + '{enter}')
-        cy.contains('.vuetable-body td', newMemberEmail)
-            .should('exist')
-            .parent()
-            .within($tr => {
-                cy.get('button[title=Impersonate]').click()
-            })
+            //login as Trainer Manager
+            cy.get('.btn__text').contains('Login').click()
+            cy.loginTrainerManager()
 
-        cy.get('.modal-active .modal-close-cross').click({force:true, multiple:true})  
+            cy.contains('Users').click()
+            cy.url().should('include', '/admin/users')
 
-        cy.get('#profile-gender').select('male')
-        cy.contains('Save Profile').click()
+            // search existing user
+            cy.get('#__BVID__16').clear().type(cEmail + '{enter}')
+            cy.contains('.vuetable-body td', cEmail)
+                .should('exist')
+                .parent()
+                .within($tr => {
+                    cy.get('button[title=Impersonate]').click()
+                })
 
-        // cy.completeWebProfile()
+            cy.get('.modal-active .modal-close-cross').click({ force: true, multiple: true })
 
-        cy.contains('Edit Profile').click()
-        cy.get('button').contains('Questionnaire').click()
+            cy.get('#profile-gender').select('male')
+            cy.contains('Save Profile').click()
 
-        cy.filloutQuestionnaire();
+            // cy.completeWebProfile()
 
-        cy.get('#menu1').contains('Admin').click()
-        
-        cy.get('.dropdown--active .menu-vertical').contains('Stop impersonating').click()        //Stop impersonating
+            cy.contains('Edit Profile').click()
+            cy.get('button').contains('Questionnaire').click()
 
-        //back in Admin dashboard
-        cy.url().should('include', 'admin/users')
-        cy.contains(newMemberEmail).should('exist')
+            cy.filloutQuestionnaire();
 
-        cy.verifyOTPInUnassignedPlansPage(
-            {
-                email : newMemberEmail
-            })
-        //assign trainer
-        cy.assignTrainer({
-            email : newMemberEmail,
-            trainer :assignTrainer
+            cy.get('#menu1').contains('Admin').click()
+
+            cy.get('.dropdown--active .menu-vertical').contains('Stop impersonating').click()        //Stop impersonating
+
+            //back in Admin dashboard
+            cy.url().should('include', 'admin/users')
+            cy.contains(cEmail).should('exist')
+
+            cy.verifyRecordNotInAssignedClientPage({ email: cEmail })
+
+            cy.verifyOTPInUnassignedPlansPage({ email: cEmail })
+            //assign trainer
+            cy.assignTrainer({ email: cEmail, trainer: assignTrainer })
+
+            //check assignment
+            cy.verifyAssignedClientOTP({ email: cEmail, trainer: assignTrainer })
+
+            //Upload PDF Plan
+            cy.uploadPlan({ email: cEmail })
+
+            //logout
+            cy.get('a[href="https://testing-2.vshred.com/logout"]').click({multiple:true, force:true})
+
+            cy.log("Custom Bundle: "+searchBundle.cartSummary+ " >> Passed")
+
         })
-
-        //check assignment
-        cy.verifyAssignedClientOTP(
-            {
-                email: newMemberEmail,
-                trainer: assignTrainer
-            })
-
-        //Upload PDF Plan
-        cy.uploadPlan(
-            {
-            email: newMemberEmail
-            })      
-
     })
 })
